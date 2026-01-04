8cbbc8f6634bd891080b1bc25daf4a00a1e3ef6d
Hemant <hemr1234@gmail.com>
2026-01-04 13:06:57 +0100

    build project

--- src/components/BurgerMenu.tsx
+++ src/components/BurgerMenu.tsx
@@ -1,15 +1,5 @@
-import {slide as Menu } from 'react-burger-menu'
-import BurgerMenu from 'react-burger-menu';
+// import BurgerMenu from 'react-burger-menu';
 
-export const MenuBurger = () => {
-  const Menu =
-    return (
-      // <Menu >
-      //   <a id="home" className="menu-item" href="/">Home</a>
-      //   <a id="about" className="menu-item" href="/about">About</a>
-      //   <a id="contact" className="menu-item" href="/contact">Contact</a>
-      //   {/* <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a> */}
-      // </Menu>
-      <BurgerMenu/>
-    );
-}
+// export const MenuBurger = () => {
+//     return <BurgerMenu/>
+// }

28e220adea807eaa54c69e42bd124b7cd2499559
Hemant <hemr1234@gmail.com>
2026-01-04 12:40:06 +0100

    fixed background image and the text body.

--- src/App.tsx
+++ src/App.tsx
@@ -7,39 +7,40 @@ import About from "./pages/About.tsx";
 import Contact from "./pages/Contact.tsx";
 import Reservations from "./pages/Reservations.tsx";
 import backgroundFire from "./assets/background-fire.jpg"
+import backgroundFireDesktop from "./assets/background-fire-wide.jpg"
+
 import logo from "./assets/logo.png"
 import background from "./assets/background.jpg"
 import "./styles/App.css";
+import { useEffect, useState } from "react";
 
 export function App() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
     <div className="app">
-      <img
-        className="hero-background"
-        src={background}
-        alt="Fire Background"
-      />
-      {/* Hero Section */}
       <header className="hero">
-        
         <div className="hero-content">
-          <img
-            className="hero-logo"
-            src={logo}
-            alt="Fire Background"
-          />
+          <img className="hero-logo" src={logo} alt="Logo" />
           <img
             className="hero-fire"
-            src={backgroundFire}
+            src={isMobile ? backgroundFire : backgroundFireDesktop}
             alt="Fire Background"
           />
         </div>
       </header>
-      {/* App Layout */}
+
       <Router>
         <div className="layout">
           <Navbar />
-
           <main className="main">
             <Routes>
               <Route path="/" element={<Home />} />
@@ -49,7 +50,6 @@ export function App() {
               <Route path="/reservations" element={<Reservations />} />
             </Routes>
           </main>
-
           <Footer />
         </div>
       </Router>
